24a2892531e207a6ae3bf34fa5c2f47502024950
CyberLight <cyberlight@live.ru>
2015-05-08 10:58:26 +0600

    Utility: Added microtime function (it's analog of microtime in PHP for Python)   * This function used for generate rnd field value

--- demirbank/utils.py
+++ demirbank/utils.py
@@ -1,1 +1,9 @@
-__author__ = 'avishnyakov'
+from time import time
+from math import modf
+
+
+def microtime(get_as_float=False):
+    if get_as_float:
+        return time()
+    else:
+        return '%f %d' % modf(time())
