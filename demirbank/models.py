480b7fdebff2632c221cc6b767d4b362592e2234
CyberLight <cyberlight@live.ru>
2015-05-11 12:35:32 +0600

    Model: Added model class for DemirBankPayment

--- demirbank/models.py
+++ demirbank/models.py
@@ -1,1 +1,38 @@
-__author__ = 'avishnyakov'
+# -*- coding: utf-8 -*-
+
+from django.db import models
+
+
+class DemirBankPayment(models.Model):
+    account = models.CharField(max_length=255)
+    added = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
+    auth_code = models.BigIntegerField(default=0)
+    extra_cardbrand = models.CharField(max_length=128, default='')
+    extra_trxdate = models.DateTimeField(null=True)
+    err_msg = models.CharField(max_length=255, default='')
+    hash = models.CharField(max_length=255, default='')
+    hash_params = models.CharField(max_length=255, default='')
+    hash_params_val = models.CharField(max_length=255, default='')
+    host_ref_num = models.CharField(max_length=50, default='')
+    masked_pan = models.CharField(max_length=50, default='')
+    pa_res_syntax_ok = models.BooleanField(default=False)
+    pa_res_verified = models.BooleanField(default=False)
+    proc_return_code = models.CharField(max_length=2, default='')
+    response = models.CharField(max_length=50, default='')
+    return_oid = models.IntegerField(default=0)
+    trans_id = models.CharField(max_length=64, default='')
+    amount = models.DecimalField(decimal_places=2, max_digits=7, default=0)
+    cavv = models.CharField(max_length=28, default='')
+    client_id = models.CharField(max_length=15, default='')
+    currency = models.PositiveSmallIntegerField(default=0)
+    eci = models.CharField(max_length=2, default='')
+    md = models.CharField(max_length=512, default='')
+    rnd = models.CharField(max_length=50, default='')
+    md_error_msg = models.CharField(max_length=512, default='')
+    md_status = models.PositiveSmallIntegerField(default=0)
+    merchant_id = models.CharField(max_length=50, default='')
+    oid = models.IntegerField(default=0)
+    storetype = models.CharField(max_length=255, default='')
+    txstatus = models.CharField(max_length=15, default='')
+    client_ip = models.GenericIPAddressField()
