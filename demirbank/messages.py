3fc4278118e09bf9773e296197bdb493773fd8e3
CyberLight <cyberlight@live.ru>
2015-05-12 09:55:28 +0600

    Messages: Added new messages for errors inside messages module

--- demirbank/messages.py
+++ demirbank/messages.py
@@ -1,3 +1,4 @@
 MESSAGE_EMPTY_HASHPARAMSVAL = "Empty HASH or HASHPARAMSVAL"
 HASH_VALUES_DO_NOT_MATCH = "Hash values do not match"
 HASHES_DO_NOT_MATCHED = "Hashes do not match"
+PAYMENT_ALREADY_PROCESSED_OR_NOT_EXISTS = "Payment already processed or not exists"
